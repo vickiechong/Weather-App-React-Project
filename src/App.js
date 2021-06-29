eed534ff0f759f23fa98f2ff3ebeb817c003c29a
vickiechong <81544596+vickiechong@users.noreply.github.com>
2021-06-29 12:27:09 +0800

    Added styling and credits

--- src/App.js
+++ src/App.js
@@ -11,11 +11,32 @@ function App() {
           href="https://github.com/vickiechong/Weather-App-React-Project"
           target="_blank"
           rel="noreferrer"
-          id="webpageaddress"
+          className="creditsgithub"
         >
-          Open-source on Github
+          Open-sourced on Github
         </a>
-        , hosted on Netify and coded by Vickie Chong
+        , hosted on{" "}
+        <a
+          href="https://tender-fermat-fa1b3b.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="creditsgithub"
+        >
+          Netify
+        </a>{" "}
+        and coded by
+        <span className="codercredits"> Vickie Chong </span>
+      </p>
+      <p className="iconstatement">
+        Icons by{" "}
+        <a
+          href="https://github.com/basmilius/weather-icons"
+          target="_blank"
+          rel="noreferrer"
+          className="creditsgithub"
+        >
+          Basmilius
+        </a>
       </p>
     </div>
   );
